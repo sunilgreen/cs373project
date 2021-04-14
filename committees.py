75f6085771a088b607b991934c9f8d1537ca0e79
Joseph Barr <barrjoeym@gmail.com>
2021-04-13 21:20:30 -0400

    add committees script/json

---
+++ committees.py
@@ -0,0 +1,7 @@
+import json
+
+import requests
+
+x = requests.get("https://api.propublica.org/congress/v1/115/senate/committees.json", headers = {'X-API-Key': '3qoRFu7imGjfAD2HoZQQuCcM3e37LbclqLe8VRv0'})
+x_obj = json.loads(x.content) 
+print(json.dumps(x_obj, indent=3))
