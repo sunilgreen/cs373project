3ae7799af260fda8a25b6b51f18323b20cc83681
Joseph Barr <barrjoeym@gmail.com>
2021-04-15 19:56:21 -0400

    spread committees/subcommittees into binary columns

--- cart.py
+++ cart.py
@@ -1,19 +1,34 @@
+import ast
 import csv
+import sys
+
+import numpy
 import pandas as pd
 import sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
-from sklearn.preprocessing import OneHotEncoder
-import sys
-import numpy
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MultiLabelBinarizer, OneHotEncoder
+from sklearn.tree import DecisionTreeClassifier
+
 numpy.set_printoptions(threshold=sys.maxsize)
 
+mlb = MultiLabelBinarizer()
 header_names = ["bill_id", "cosponsors", "sponsor_party", "sponsor_state", "committee_codes", "subcommittee_codes", "primary_subject", "is_pork"]
 df = pd.read_csv("billdata.csv")
 
+# In csv file, column values are strings, but we must parse them as lists
+df["committee_codes"] = df["committee_codes"].transform(lambda x: ast.literal_eval(x))
+df["subcommittee_codes"] = df["subcommittee_codes"].transform(lambda x: ast.literal_eval(x))
+
+# Transform committe_codes and subcommittee_codes into several columns, such that e.g. HSFA, HSGO, etc. have their own binary column.
+# So if a particular bill has HSFA as a committee, it has a 1 in the HSFA column. Otherwise it has a 0.
+df = df.join(pd.DataFrame(mlb.fit_transform(df["committee_codes"]), columns=mlb.classes_, index=df.index))
+df = df.join(pd.DataFrame(mlb.fit_transform(df["subcommittee_codes"]), columns=mlb.classes_, index=df.index))
+
+df = df.drop(["committee_codes", "subcommittee_codes"], axis=1)
+
 enc = OneHotEncoder(handle_unknown='ignore')
-X = df[['sponsor_party', 'sponsor_state', 'committee_codes']]
+X = df[['sponsor_party', 'sponsor_state']]
 enc.fit(X)
 X = enc.transform(X).toarray()
 print(X.shape)
@@ -30,4 +45,3 @@ clf = DecisionTreeClassifier()
 clf = clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
 #print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-    
